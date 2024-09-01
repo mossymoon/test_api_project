66ba46821527db7fa5a0e4fdf1fe59fada777902
mossymoon <@stepanobuelo>
2024-09-01 18:07:08 +0300

    initial commit

---
+++ conftest.py
@@ -0,0 +1,58 @@
+import requests
+import logging
+import pytest
+from utils import post
+
+url_post = "https://api.restful-api.dev/objects"
+url_put = "https://api.restful-api.dev/objects"
+url_get = "https://api.restful-api.dev/objects"
+url_get_id = "https://api.restful-api.dev/objects"
+
+
+@pytest.fixture()
+def configure_logging():
+    logging.basicConfig(
+        level=logging.DEBUG,
+        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+        handlers=[
+            logging.FileHandler('logs/log'),  # Log file
+            logging.StreamHandler()
+        ]
+    )
+    logger = logging.getLogger(__name__)
+    logger.info("Logging setup complete")
+
+
+@pytest.fixture()
+def post_method(data_for_json, configure_logging):
+    response = post(url=url_post, json=data_for_json)
+
+    return response
+
+
+@pytest.fixture()
+def put_method(data_for_json, id, color):
+    data = data_for_json
+    data['data'].update({"color": color})
+    req = requests.put(url=f"{url_put}/{id}", json=data)
+    return req
+
+
+@pytest.fixture()
+def patch_method(name, id):
+    data = {"name": name}
+    req = requests.patch(url=f"{url_put}/{id}", json=data)
+    return req
+
+
+@pytest.fixture()
+def get_method():
+    response = requests.get(url=url_get)
+    return response
+
+
+@pytest.fixture()
+def get_method_id(id):
+    params = {"id": id}
+    response = requests.get(url=url_get, params=params)
+    return response
